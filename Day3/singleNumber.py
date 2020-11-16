b76838a346f1bcda5f3a3c08642efe75c2a9dbcb
Dhruv Mehta <39973939+Dhruvmehta18@users.noreply.github.com>
2020-11-16 14:58:15 +0530

    added the singleNumber problem

---
+++ Day3/singleNumber.py
@@ -0,0 +1,16 @@
+# Hi, here's your problem today. This problem was recently asked by Facebook:
+
+# Given a list of numbers, where every number shows up twice except for one number, find that one number.
+
+# Example:
+# Input: [4, 3, 2, 4, 1, 3, 2]
+# Output: 1
+
+def singleNumber(nums):
+    ans = 0
+    for x in nums:
+        ans = ans ^ x
+    return ans
+
+print(singleNumber([4, 3, 2, 4, 1, 3, 2]))
+# 1
